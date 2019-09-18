33d469538bdc36bb5bbbfe4f4e0367af91f5062b
muraahm <ahmedmuraahm121@gmail.com>
2019-09-18 15:59:45 +0000

    add admin porducts and categories counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class Admin::DashboardController < ApplicationController
   def show
+    @products_count = Product.all.order(created_at: :desc).count
+
+    @category_count = Category.all.count
   end
 end
