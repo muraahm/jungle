c867e22d811d07b484fccd42f5d617e66161176c
muraahm <ahmedmuraahm121@gmail.com>
2019-09-19 18:01:57 +0000

    modify show cart to allow guests to place orders

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -20,7 +20,6 @@ class CartsController < ApplicationController
 
   private
 
-
   def modify_cart_delta(product_id, delta)
     cart[product_id] = (cart[product_id] || 0) + delta
     cart.delete(product_id) if cart[product_id] < 1
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ActiveRecord::Base
   validates :email, presence: true
   validates :password, presence: true
   validates :password_confirmation, presence: true
+
+  
 end
