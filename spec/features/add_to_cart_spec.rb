aa044275dd847340ba20f0b3525492863222852f
muraahm <ahmedmuraahm121@gmail.com>
2019-09-24 15:59:35 +0000

    testing add to cart number in nav

---
+++ spec/features/add_to_cart_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    1.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        quantity: 10,
+        price: 64.99,
+        image: open_asset('apparel1.jpg')
+      )
+    end
+  end
+
+  scenario "They see all products" do
+    # ACT
+    visit root_path
+    find('article.product form').click
+
+    # DEBUG / VERIFY
+    save_screenshot
+    within('nav') { expect(page).to have_content('My Cart (1)') }
+  end
+
+end
